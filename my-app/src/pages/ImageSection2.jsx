c66dc55554f0e9b8672fae1604c3aadfb263666b
angelik323 <angelica.loaiza323@gmail.com>
2022-10-28 18:00:18 -0500

    Section2 modificado: Se crean dos columnas para la seccion 2 donde va a estar al lado izquierdo una imagen y al lado derecho información de la misma, se crea componente a parte de la imagen y se importa en el app.js

---
+++ my-app/src/pages/ImageSection2.jsx
@@ -0,0 +1,10 @@
+import '../css/imageSection2.css';
+import imgSection2 from '../assets/img/img-section2.png';
+
+function ImageSection2() {
+  return (
+    <img src={imgSection2} className="sizeImgSrction2 navbar-brand" alt="imagen de una universidad" />
+  );
+}
+
+export default ImageSection2;
