11fc99461541b44a122f1d24af03625bb09bff81
angelik323 <angelica.loaiza323@gmail.com>
2022-10-29 01:30:20 -0500

    cambios en Services y otros componentes

--- my-app/src/pages/Section2.jsx
+++ my-app/src/pages/Section2.jsx
@@ -4,21 +4,20 @@ import ImageFlotante from './ImageSection2';
 
 function Section2() {
   return (
-    <div class="container containerSection">
+    <div id='our' class="container containerSection">
       <div class="row">
         <div class="p-rel col-6 col-md-4 my-4">
-          <h1>Lorenzo Morena</h1>
-          <span>Founder</span>
+          <h1>Stella Studio University</h1>
           <ImageFlotante />
         </div>
         <div class="col-md-8">
           <h1 className='titleSizeSection2'>We propel results-driven growth</h1>
           
           <p className='my-4'>
-            The Massachusetts Institute of Technology (MIT) is a private land-grant research university in Cambridge, Massachusetts. Established in 1861, MIT has since played 
-            a key role in the development of modern technology and science, ranking among the top academic institutions in the world.[10][11][12][13]
+            The Stella Studio University is a private land-grant research university in Colombia, Audifarma. Established in 1861, SSU has since played 
+            a key role in the development of modern technology and science, ranking among the top academic institutions in the world.
 
-            Founded in response to the increasing industrialization of the United States, MIT adopted a European polytechnic university model and stressed laboratory instruction 
+            Founded in response to the increasing industrialization of the Colombia, SSU adopted a European polytechnic university model and stressed laboratory instruction 
             in applied science and engineering. 
           </p>
           <button type="button" class="btn buttonColor2 btn-lg my-4">View Detail</button>
