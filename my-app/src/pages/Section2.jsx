d540ecd9879066ef1c23c8089da0afa8048ad14a
angelik323 <angelica.loaiza323@gmail.com>
2022-10-28 23:00:02 -0500

    modificaciones section2

--- my-app/src/pages/Section2.jsx
+++ my-app/src/pages/Section2.jsx
@@ -4,10 +4,10 @@ import ImageFlotante from './ImageSection2';
 
 function Section2() {
   return (
-    <div class="container">
+    <div class="container containerSection">
       <div class="row">
         <div class="p-rel col-6 col-md-4 my-4">
-          <h2>Lorenzo Morena</h2>
+          <h1>Lorenzo Morena</h1>
           <span>Founder</span>
           <ImageFlotante />
         </div>
